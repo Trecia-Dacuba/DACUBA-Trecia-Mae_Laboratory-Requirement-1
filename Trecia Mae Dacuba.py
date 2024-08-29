1836350c51bf296a3cb5558af65d781acf90e4a5
Trecia-Dacuba <treciamae.dacuba@gmail.com>
2024-08-29 03:44:46 -0700

    Add files via upload

---
+++ Trecia Mae Dacuba.py
@@ -0,0 +1,7 @@
+import pyfiglet
+
+text=pyfiglet.print_figlet(text="Trecia Mae", colors='yellow', font="electronic")
+
+text=pyfiglet.print_figlet(text="Dacuba", colors='blue', font="electronic")
+
+print(text)
